8232d779ae9a468c5097d6c771689ffa95ca1c72
PCM34 <32440304+PCM34@users.noreply.github.com>
2020-04-28 10:49:19 -0500

    I updated the code so that it can get the date and time.

--- app/src/main/java/com/example/restaurantfinder/MainActivity.java
+++ app/src/main/java/com/example/restaurantfinder/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.restaurantfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button b = findViewById(R.id.search_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                int day = now.get(Calendar.DAY_OF_WEEK);
+                int hour = now.get(Calendar.HOUR_OF_DAY);
+                int minutes = now.get(Calendar.MINUTE);
+                TextView t = findViewById(R.id.tester);
+                if (day == 1) {
+                    t.setText("Sunday" + " " + hour + " : " + minutes);
+                } else if (day == 2) {
+                    t.setText("Monday" + " " + hour + " : " + minutes);
+                } else if (day == 3) {
+                    t.setText("Tuesday" + " " + hour + " : " + minutes);
+                } else if (day == 4) {
+                    t.setText("Wednesday" + " " + hour + " : " + minutes);
+                } else if (day == 5) {
+                    t.setText("Thursday" + " " + hour + " : " + minutes);
+                } else if (day == 6) {
+                    t.setText("Friday" + " " + hour + " : " + minutes);
+                } else if (day == 7) {
+                    t.setText("Saturday" + " " + hour + " : " + minutes);
+                }
+            }
+        });
     }
 }
